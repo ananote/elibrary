642ff434255630bac25ad67dfd71513fd8c55167
ananote <amit.nanote@impetus.co.in>
2014-10-31 09:49:56 +0530

    clean commit

--- src/main/java/com/impetus/elibrary/controller/UserEmailController.java
+++ src/main/java/com/impetus/elibrary/controller/UserEmailController.java
@@ -1,7 +1,6 @@
 package com.impetus.elibrary.controller;
 
 import java.lang.reflect.Field;
-import java.util.Date;
 import java.util.List;
 import java.util.logging.Logger;
 
--- src/main/java/com/impetus/elibrary/utils/ModelUtils.java
+++ src/main/java/com/impetus/elibrary/utils/ModelUtils.java
@@ -3,7 +3,6 @@ package com.impetus.elibrary.utils;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import com.google.common.collect.ArrayListMultimap;
